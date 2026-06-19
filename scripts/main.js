a8dbbce98b1d1aee6b4a32e07ea3a4e897a31964
Brandon Fitzpatrick <113076498+BrandonFitzpatricc@users.noreply.github.com>
2026-06-19 12:11:36 -0400

    Write tree demo in main module

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,65 @@
 import { Tree } from "./tree.js";
 
-const tree = new Tree([5]);
-tree.insert(4);
-tree.insert(3);
-tree.insert(2);
-tree.insert(1);
-tree.insert(6);
-tree.insert(7);
-tree.insert(8);
-tree.insert(9);
-tree.isBalanced();
-tree.prettyPrint();
+const tree = new Tree(getRandomArray(25));
+
+console.log(tree.prettyPrint());
+console.log();
+
+console.log(`Is Balanced: ${tree.isBalanced()}`);
+console.log();
+
+console.log("Level Order Traversal:");
+tree.levelOrderForEach((value) => console.log(value));
+console.log();
+
+console.log("Pre Order Traversal:");
+tree.preOrderForEach((value) => console.log(value));
+console.log();
+
+console.log("Post Order Traversal:");
+tree.postOrderForEach((value) => console.log(value));
+console.log();
+
+console.log("In Order Traversal:");
+tree.inOrderForEach((value) => console.log(value));
+console.log();
+
+tree.insert(105);
+tree.insert(110);
+tree.insert(115);
+tree.insert(120);
+
+console.log(`Is Balanced (after unbalancing): ${tree.isBalanced()}`);
+console.log();
+
+tree.rebalance();
+
+console.log(`Is Balanced (after rebalancing): ${tree.isBalanced()}`);
+console.log();
+
+console.log(tree.prettyPrint());
+console.log();
+
+console.log("Level Order Traversal:");
+tree.levelOrderForEach((value) => console.log(value));
+console.log();
+
+console.log("Pre Order Traversal:");
+tree.preOrderForEach((value) => console.log(value));
+console.log();
+
+console.log("Post Order Traversal:");
+tree.postOrderForEach((value) => console.log(value));
+console.log();
+
+console.log("In Order Traversal:");
+tree.inOrderForEach((value) => console.log(value));
+console.log();
+
+function getRandomArray(size) {
+  const arr = [];
+  for (let i = 0; i < size; i++) {
+    arr.push(Math.floor(100 * Math.random()));
+  }
+  return arr;
+}
